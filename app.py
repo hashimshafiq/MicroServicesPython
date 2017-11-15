94efc62ecf4663d4e118744f1381d1c68ceadf66
hashimshafiq <hashimshafiq0@gmail.com>
2017-11-15 17:36:07 +0500

    API v2 added and completed

--- app.py
+++ app.py
@@ -4,11 +4,103 @@ from flask import make_response
 from flask import abort
 from flask import request
 import json
+from time import strftime
+from time import gmtime
 import sqlite3
 
 
 path = "D:/OpenSource/MicroServicesPython/micro.db"
 app = Flask(__name__)
+
+@app.route("/api/v2/info")
+def home_index2():
+
+	conn = sqlite3.connect(path)
+	print("Database opened")
+	api_list = []
+	cursor = conn.execute("SELECT buildtime, version,methods, links from apirelease")
+
+	for row  in cursor:
+		a_dict = {}
+		a_dict['version'] = row[1]
+		print(row)
+		a_dict['buildtime'] = row[0]
+		a_dict['methods'] = row[2]
+		a_dict['links'] = row[3]
+		api_list.append(a_dict)
+
+	conn.close()
+	return jsonify({'api_version':api_list}),200
+
+@app.route("/api/v2/tweets",methods=['GET'])
+def get_tweets():
+	return list_tweets()
+
+
+def list_tweets():
+	conn = sqlite3.connect(path)
+	api_list=[]
+	cursor = conn.execute("SELECT username, body, tweet_time, id FROM tweets")
+	data = cursor.fetchall()
+	if data != 0:
+		for row in data:
+			tweets = {}
+			tweets['Tweet By'] = row[0]
+			tweets['Body'] = row[1]
+			tweets['Timestamp'] = row[2]
+			tweets['id'] = row[3]
+			api_list.append(tweets)
+
+	conn.close()
+	return jsonify({'tweets_list': api_list}),200
+
+@app.route("/api/v2/tweets",methods=['POST'])
+def add_tweets():
+	user_tweet = {}
+	if not request.json or not 'username' in request.json or not 'body' in request.json:
+		abort(400)
+	user_tweet['username'] = request.json['username']
+	user_tweet['body'] = request.json['body']
+	user_tweet['created_at'] = strftime("%Y-%m-%dT%H:%M:%SZ", gmtime())
+	print(user_tweet)
+	return jsonify({'status':add_tweet(user_tweet)}),201
+
+def add_tweet(new_tweets):
+	conn = sqlite3.connect(path)
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM users WHERE username=?",(new_tweets['username'],))
+	data = cursor.fetchall()
+	if len(data) == 0:
+		abort(404)
+	else:
+		cursor.execute("INSERT into tweets (username,body,tweet_time) VALUES(?,?,?)",(new_tweets['username'],new_tweets['body'],new_tweets['created_at']))
+		conn.commit()
+	return "success"
+
+
+
+@app.route('/api/v2/tweets/<int:id>', methods=['GET'])
+def get_tweet(id):
+	return list_tweet(id)
+
+def list_tweet(user_id):
+	conn = sqlite3.connect(path)
+	api_list=[]
+	cursor=conn.cursor()
+	cursor.execute("SELECT * from tweets where id=?",(user_id,))
+	data = cursor.fetchall()
+	if len(data) == 0:
+		abort(404)
+	else:
+		user = {}
+		user['id'] = data[0][0]
+		user['username'] = data[0][1]
+		user['body'] = data[0][2]
+		user['tweet_time'] = data[0][3]
+	conn.close()
+	return jsonify(user)
+
+
 
 @app.route("/api/v1/info")
 def home_index():
